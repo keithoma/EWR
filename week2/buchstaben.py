f46176a1946f3169d576527e133245c797cebd91
keithoma <thomakmj@gmail.com>
2019-04-19 10:58:46 +0200

    Update buchstaben.py
    fixed pylint's issues

--- week2/buchstaben.py
+++ week2/buchstaben.py
@@ -2,8 +2,8 @@
 """
     20190416
 
-    This module was written for the EWR course at Humboldt University. Given a string, it returns the
-    number of vowels in the string.
+    This module was written for the EWR course at Humboldt University. Given a string, it returns
+    the number of vowels in the string.
 
     Module was constructed by:
     Christian Parpart (185676)
@@ -18,9 +18,9 @@ def count_vowels(sentence):
         Returns:
             (int): the number of vowels in sentence
     """
-    VOWEL_LIST = 'aeiou'
+    vowel_list = 'aeiou'
 
-    return sum(letter in VOWEL_LIST for letter in sentence.lower())
+    return sum(letter in vowel_list for letter in sentence.lower())
 
 def main():
     """
