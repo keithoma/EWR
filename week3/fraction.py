9dd05bdf103f96cdc95584335843a061212f8446
Half Life Plot <thomakmj@gmail.com>
2019-05-07 13:12:45 +0200

    there was an error in lcm() function; fixed it

--- week3/fraction.py
+++ week3/fraction.py
@@ -1,49 +1,61 @@
 #! /usr/bin/env python3
 """
     constructed by:
-    Christian Parpart (XXX XXX)
-    Kei Thoma (574613)
+    Christian Parpart (18 56 76)
+    Kei Thoma (574 613)
 """
 
-import euclidean_algorithm
-
-class Fraction:
+def euclidean_algorithm(first_number, second_number):
     """
-        OMG FILL ME NOW, BABY ONE MORE TIME. :-*
+        This function finds the greatest common divisor with the help of recursive euclidean
+        algorithm.
+
+        Args:
+            first_number (int): the first number
+            second_number (int): the second number
+
+        Returns:
+            (int): the greatest common divisor of 'first_number' and 'second_number'
     """
-    def __init__(self, numerator, denominator=1):
-        if denominator == 0:
-            raise ZeroDivisionError
 
-        self.numerator = abs(numerator)
-        self.denominator = abs(denominator)
+    # first of all, break the cycle if we already have a zero
+    # notice also that without this, we'd get a zero-division error
+    if second_number == 0:
+        return first_number
 
-        # set the sign, True if positive (or zero) and False if negative
-        self.sign = False
-        if numerator * denominator >= 0:
-            self.sign = True
+    # this is the actual part of the algorithm
+    # input the 'second_number' as the new first argument and modify the second argument
+    return euclidean_algorithm(second_number, first_number % second_number)
 
-        # finally, reduce the fraction
-        self.reduce_fraction()
+def least_common_multiple(first_number, second_number):
+    """
+        This one calculates the least common multiple via the greatest common divisor through the
+        well known formula, lcm(a, b) = abs(ab) / gcd(a, b).
 
-    def reduce_fraction(self):
-        """
-            This method reduces the fraction using the euclidean algorithm implemented in
-            'euclidean_algorithm.py'.
-        """
-        gcd = euclidean_algorithm.euclidean_algorithm(self.numerator, self.denominator)
-        if gcd > 1:
-            self.numerator = int(self.numerator / gcd)
-            self.denominator = int(self.denominator / gcd)
+        Args:
+            first_number (int): in the formula this is 'a'
+            second_number (int): in the forumula this is 'b'
+
+        Returns:
+            (int): the least common divisor
+    """
+
+    # we will break up the calculation in few steps due to limited space
+    gcd = euclidean_algorithm(first_number, second_number)
+    numerator = abs(first_number * second_number)
+    return int(numerator / gcd)
 
 def main():
     """
-        Here be dragons.
+        The main-function for testing purposes.
     """
-    test_fraction = Fraction(-15, -3)
-    print(test_fraction.numerator)
-    print(test_fraction.denominator)
-    print(test_fraction.sign)
+    print("We will do some tests:")
+    print("gcd(195, 1287) = " + str(euclidean_algorithm(195, 1287)) + " (should be 39)")
+    print("gcd(13, 17) = " + str(euclidean_algorithm(13, 17)) + " (should be 1)")
+    print("gcd(24, 24) = " + str(euclidean_algorithm(24, 24)) + " (should be 24)")
+    print("gcd(0, 1) = " + str(euclidean_algorithm(0, 1)) + " (should be 1)")
+    print("gcd(0, 2) = " + str(euclidean_algorithm(0, 2)) + " (should be 2)")
+    print("gcd(12, 0) = " + str(euclidean_algorithm(12, 0)) + " (should be 12)")
 
 if __name__ == "__main__":
     main()
