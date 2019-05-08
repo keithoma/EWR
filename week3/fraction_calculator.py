87806869fea9674b694f197652f7e61f3e28e67a
Half Life Plot <thomakmj@gmail.com>
2019-05-08 18:00:38 +0200

    should hopefully work
    remember kids, raw_input is python2

--- week3/fraction_calculator.py
+++ week3/fraction_calculator.py
@@ -11,13 +11,39 @@ import fraction
 
 
 
-def take_user_input():
-    pass
+def interpret(user_input):
+    """
+        Args:
+            input (string): the string entered to the console by the user
+
+        Returns:
+            (Fraction): the Fraction object constructed by the user's input
+
+        Raise:
+            ValueError: if the input has multiple separators
+    """
+    user_input = user_input.split("/")
+
+    if len(user_input) > 2:
+        raise ValueError("I can't read that.")
+
+    return fraction.Fraction(int(user_input[0]), int(user_input[1]))
 
 
 
 def main():
-    pass
+    """
+        Our looping calculator.
+    """
+    while True:
+        print("I can add two fractions!")
+
+        solution = interpret(input("The first fraction: ")) +
+                   interpret(input("The second fraction: "))
+        print("The solution is: {0} \n\n".format(solution))
+
+        if input("Exit?").lower()[0] == "y":
+            break
 
 
 
