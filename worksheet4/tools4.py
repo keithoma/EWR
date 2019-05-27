e83d3acc8e919a69b18ce865044d5b840bd81c4e
keithoma <thomakmj@gmail.com>
2019-05-27 18:02:09 +0200

    machine precision test

---
+++ worksheet4/tools4.py
@@ -0,0 +1,41 @@
+"""
+    forged by:
+    Christian Parpart
+    Kei Thoma (574 613)
+"""
+
+import numpy as np
+
+def explore_machine_epsilon(float_type):
+    """
+        This little algorithm tries to find the machine precision of the given float type such as
+        np.float16 iterativly.
+
+        Arguments:
+            float_type (class): the class for the float type we want to inspect; e.g. np.float32
+        
+        Returns:
+            epsilon (float_type): the machine precision; its data type corresponds to the data type passed
+                                  as the argument
+    """
+    epsilon = float_type(1.0)
+
+    while float_type(1.0 + epsilon * 0.5) != 1.0:
+        epsilon = float_type(epsilon * 0.5)
+
+    return epsilon
+
+def main():
+    def print_experiment(float_type_list):
+        for each in float_type_list:
+            # padding in the second line added for readability 
+            # "__name__" returns the name of the class; such as "np.float16"
+            print("The machine epsilon for {0} is:".format(each.__name__))
+            print("              {0}".format(explore_machine_epsilon(each)))
+            print("it should be: {0}\n".format(np.finfo(each).eps))
+
+    list_to_test = [np.float16, np.float32, np.float64]
+    print_experiment(list_to_test)
+
+if __name__ == "__main__":
+    main()
